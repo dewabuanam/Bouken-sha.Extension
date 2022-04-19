cfe40ea86b3604f99a1501dd26a3e4f5ff849aa1
Dewa Buana Muharmadin <dewa.muharmadin@m2.formulatrix.com>
2022-04-19 21:43:23 +0700

    initial commit

---
+++ Bouken-sha.Extension.KissAnime/Bouken-sha.Extension.KissAnime/AnimeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bouken_sha.Extension.Models;
+using Bouken_sha.Extension.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouken_sha.Extension.KissAnime
+{
+    public class AnimeRepository : IAnimeRepository
+    {
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Anime>> FindAsync(AnimeFilter filter, AnimeOrder order, string search, int page, int perPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Anime>> GetAsync(AnimeOrder order, int page, int perPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Anime> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Anime> UpsertAsync(Anime anime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bouken-sha.Extension.KissAnime/Bouken-sha.Extension.KissAnime/EpisodeRepository.cs
@@ -0,0 +1,36 @@
+﻿using Bouken_sha.Extension.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouken_sha.Extension.KissAnime
+{
+    public class EpisodeRepository : IEpisodeRepository
+    {
+        public Task DeleteAsync(string animeId, string epsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Episode>> FindAsync(string animeId, EpisodeFilter filter, EpisodeOrder order, string epsId, int page, int perPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Episode>> GetAsync(string animeId, EpisodeOrder order, int page, int perPage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Episode> GetAsync(string animeId, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Episode> UpsertAsync(string animeId, Episode eps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
